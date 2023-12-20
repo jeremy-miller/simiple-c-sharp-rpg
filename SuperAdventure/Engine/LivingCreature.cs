be8d67ec6d298ca72eeb5d2b494bb8347a4d77e7
Jeremy Miller <jeremymiller@fastmail.com>
2023-12-19 21:17:42 -0800

    Add final code

---
+++ SuperAdventure/Engine/LivingCreature.cs
@@ -0,0 +1,14 @@
+﻿namespace Engine
+{
+    public class LivingCreature
+    {
+        public int MaximumHitPoints { get; set; }
+        public int CurrentHitPoints { get; set; }
+
+        public LivingCreature(int currentHitPoints, int maximumHitPoints)
+        {
+            CurrentHitPoints = currentHitPoints;
+            MaximumHitPoints = maximumHitPoints;
+        }
+    }
+}
---
+++ SuperAdventure/Engine/PlayerQuest.cs
@@ -0,0 +1,13 @@
+﻿namespace Engine
+{
+    public class PlayerQuest
+    {
+        public Quest Details { get; set; }
+        public bool IsCompleted { get; set; }
+        public PlayerQuest(Quest details)
+        {
+            Details = details;
+            IsCompleted = false;
+        }
+    }
+}
